b9894f887b54328823d8c327794a87d464b2d113
Junzhou Zhao <jzzhao@sei.xjtu.edu.cn>
2014-09-20 15:12:02 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -20,14 +20,15 @@ const static int W = 1000;
 const static int M = 200;
 const static int N = 27770;
 const static double PEdge = 0.2;
+const static int PerRpt=10;
 
-void em_sub(TFltV& AvgThV, int& NSuc, ExamMgr& ExM, const int Rpt){
+void em_sub(TFltV& ThV, int& NSuc, ExamMgr& ExM){
 	const double PDelta = pow(PEdge, 3);
 	NSuc = 0;
 	PNEGraph G = PNEGraph::TObj::New();
 	TIntPrV TridCnt;
 	TIntH Freq;
-	for (int rpt=0; rpt<Rpt; rpt++){
+	for (int rpt=0; rpt<PerRpt; rpt++){
 		printf("************ rpt = %d ************\n", rpt);
 		G->Clr(); TridCnt.Clr(); Freq.Clr();
 		printf("Stating sampled data...\n");
@@ -42,24 +43,23 @@ void em_sub(TFltV& AvgThV, int& NSuc, ExamMgr& ExM, const int Rpt){
 		TCEM EM(W, M, N, PDelta, Freq);
 		printf("Em running...\n");
 		if (EM.Run()) {
-			for (int i=0; i<=W; i++) AvgThV[i] += EM.ThV[i];
+			for (int i=0; i<=W; i++) ThV[i] += EM.ThV[i];
 			NSuc++;
 		}
 	}
-	printf("Experiment repeats %d times, and %d succeeded.\n", Rpt, NSuc);
+	printf("Experiment repeats %d times, and %d succeeded.\n", PerRpt, NSuc);
 }
 
 void em_multi(){
 	ExamMgr ExM(GFNm, PEdge);
-	int PerRpt=1;
-	TFltV AvgThV1(W+1), AvgThV2(W+1), AvgThV3(W+1), AvgThV4(W+1), AvgThV5(W+1);
+	TFltV ThV1(W+1), ThV2(W+1), ThV3(W+1), ThV4(W+1), ThV5(W+1);
 	int NSuc1=0, NSuc2=0, NSuc3=0, NSuc4=0, NSuc5=0;
 	std::vector<std::function<void()>> vec {
-		[&AvgThV1, &NSuc1, &ExM, PerRpt] () { em_sub(AvgThV1, NSuc1, ExM, PerRpt); },
-		[&AvgThV2, &NSuc2, &ExM, PerRpt] () { em_sub(AvgThV2, NSuc2, ExM, PerRpt); },
-		[&AvgThV3, &NSuc3, &ExM, PerRpt] () { em_sub(AvgThV3, NSuc3, ExM, PerRpt); },
-		[&AvgThV4, &NSuc4, &ExM, PerRpt] () { em_sub(AvgThV4, NSuc4, ExM, PerRpt); },
-		[&AvgThV5, &NSuc5, &ExM, PerRpt] () { em_sub(AvgThV5, NSuc5, ExM, PerRpt); },
+		[&ThV1, &NSuc1, &ExM] () { em_sub(ThV1, NSuc1, ExM); },
+		[&ThV2, &NSuc2, &ExM] () { em_sub(ThV2, NSuc2, ExM); },
+		[&ThV3, &NSuc3, &ExM] () { em_sub(ThV3, NSuc3, ExM); },
+		[&ThV4, &NSuc4, &ExM] () { em_sub(ThV4, NSuc4, ExM); },
+		[&ThV5, &NSuc5, &ExM] () { em_sub(ThV5, NSuc5, ExM); },
 	};
 	std::vector<std::thread> threads;
 	for(const auto& f: vec) threads.emplace_back((std::function<void()>)f);
@@ -67,11 +67,12 @@ void em_multi(){
 
 	printf("Saving...\n");
 	for (int i=0; i<=W; i++) {
-		AvgThV1[i] += (AvgThV2[i] + AvgThV3[i] + AvgThV4[i] + AvgThV5[i]);
-		AvgThV1[i] /= (NSuc1 + NSuc2 + NSuc3 + NSuc4 + NSuc5);
+		ThV1[i] += (ThV2[i] + ThV3[i] + ThV4[i] + ThV5[i]);
+		ThV1[i] /= (NSuc1 + NSuc2 + NSuc3 + NSuc4 + NSuc5);
 	}
-	TStr OFnm = TStr::Fmt("est_%s_p%g_r%d.dist", GFNm.GetFMid().CStr(), PEdge, PerRpt*vec.size());
-	BIO::SaveFltsWithIdx(AvgThV1, OFnm);
+	TStr OFnm = TStr::Fmt("est_%s_p%g_r%d.dist", GFNm.GetFMid().CStr(),
+		PEdge, PerRpt*vec.size());
+	BIO::SaveFltsWithIdx(ThV1, OFnm);
 	printf("Saved to %s\n", OFnm.CStr());
 }
 
