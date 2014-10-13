8b5197ca674d2f3c503a09aafbf5915cc0f4568f
jzzhao <jzzhao@sei.xjtu.edu.cn>
2014-10-13 22:19:27 +0800

    auto

--- main_th.cpp
+++ main_th.cpp
@@ -26,7 +26,7 @@ void em_sub(TFltV& ThV, int& NSuc, ExamMgr& ExM){
 		TridCnt.Clr();
 		TSnap::GetTriadParticipAll(G, TridCnt);
 		TCEM EM(ExM.W, ExM.N, pow(ExM.PEdge, 3), TridCnt);
-		printf("Sampled:\t nodes: %d, edges: %d, M: %d\n", G->GetNodes(), G->GetEdges(), EM.M);
+		printf("Sampled: nodes: %d, edges: %d, M: %d\n", G->GetNodes(), G->GetEdges(), EM.M);
 		if (EM.Run()) {
 			for (int i=0; i<=ExM.W; i++) ThV[i] += EM.ThV[i];
 			NSuc++;
