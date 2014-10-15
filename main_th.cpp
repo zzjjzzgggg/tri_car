8a107d6ab3d10f2e48f34a7b4a1c51201aa4b8e8
jzzhao <jzzhao@sei.xjtu.edu.cn>
2014-10-15 14:34:04 +0800

    auto

--- main_th.cpp
+++ main_th.cpp
@@ -69,7 +69,7 @@ int main(int argc, char* argv[]){
 	const TStr GFNm = Env.GetIfArgPrefixStr("-i:", "", "Input graph");
 	const int W = Env.GetIfArgPrefixInt("-w:", 1000, "W");
 	const int CPU = Env.GetIfArgPrefixInt("-c:", 8, "Cores to use, max=8");
-	const int Rpt = Env.GetIfArgPrefixInt("-r:", 12, "Repeat");
+	const int Rpt = Env.GetIfArgPrefixInt("-r:", 12, "Repeat times");
 	const double Pe = Env.GetIfArgPrefixFlt("-p:", 0.1, "Edge sampling rate");
 	if (Env.IsEndOfRun()) return 0;
 
