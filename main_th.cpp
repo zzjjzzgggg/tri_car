abd436e06e54d7c427ed42afe485c8376ea0440b
jzzhao <jzzhao@sei.xjtu.edu.cn>
2014-10-15 11:48:48 +0800

    auto

--- main_th.cpp
+++ main_th.cpp
@@ -66,12 +66,14 @@ void em_multi(ExamMgr& ExM){
 
 int main(int argc, char* argv[]){
 	Env = TEnv(argc, argv, TNotify::StdNotify);
-	Env.PrepArgs();
-	const TStr GFNm = Env.GetIfArgPrefixStr("-i:", "../hepth/cit-HepTh.gz", "Input graph");
+	Env.PrepArgs(TStr::Fmt("Build: %s, %s. Time: %s", __TIME__, __DATE__, TExeTm::GetCurTm()));
+	const TStr GFNm = Env.GetIfArgPrefixStr("-i:", "", "Input graph");
 	const int W = Env.GetIfArgPrefixInt("-w:", 1000, "W. Default 1000");
 	const int CPU = Env.GetIfArgPrefixInt("-c:", 8, "Cores. Default 8");
 	const int Rpt = Env.GetIfArgPrefixInt("-r:", 12, "Repeat. Default 12");
 	const double Pe = Env.GetIfArgPrefixFlt("-p:", 0.1, "Edge sampling rate. Default 0.1");
+	if (Env.IsEndOfRun()) return 0;
+
 	TExeTm2 tm;
 	ExamMgr ExM(GFNm, W, Pe, CPU, Rpt);
 	em_multi(ExM);
